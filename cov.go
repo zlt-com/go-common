36269822141cf4598e8656df01880a0f656ca587
zlt-com <1228753154@qq.com>
2021-06-29 14:38:05 +0800

    Update cov.go

--- cov.go
+++ cov.go
@@ -228,3 +228,21 @@ func String(i interface{}) string {
 		return "unknow type"
 	}
 }
+
+//数组去重
+func RemoveDuplicatesAndEmpty(arr []string) (newArr []string) {
+	newArr = make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
+}
