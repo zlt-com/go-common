9f554ff9773d408e3f9ec6979e11e84265c6e364
jerry lew <you@example.com>
2021-09-28 17:27:37 +0800

    增加方法

--- cov.go
+++ cov.go
@@ -246,3 +246,21 @@ func RemoveDuplicatesAndEmpty(arr []string) (newArr []string) {
 	}
 	return
 }
+
+/**
+ * @Description:
+ * @User: zlt-com <nohairs@gmail.com>
+ * @Date: 2021-09-27 16:55:17
+ * @param {map[string]interface{}} m
+ * @return {*}
+ */
+func GetKeys(m map[string]interface{}) []string {
+	// 数组默认长度为map长度,后面append时,不需要重新申请内存和拷贝,效率很高
+	j := 0
+	keys := make([]string, len(m))
+	for k := range m {
+		keys[j] = k
+		j++
+	}
+	return keys
+}
